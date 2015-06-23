10971ebbd8ea85b338e541c33fb28faa59b6c80a
Ricatdo Luis Baumann <ricardo.luis.baumann@gmail.com>
2015-06-23 11:45:52 -0300

    project setup

---
+++ AndroidSMSApp/app/src/main/java/com/javapapers/android/androidsmsapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.javapapers.android.androidsmsapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goToInbox(View view) {
+        Intent intent = new Intent(MainActivity.this, ReceiveSmsActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToCompose(View view) {
+        Intent intent = new Intent(MainActivity.this, SendSmsActivity.class);
+        startActivity(intent);
+    }
+
+}
